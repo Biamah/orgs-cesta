fb9186615daf74f401714630b68d6687299a1946
Biamah <beatriz.souza.costa@outlook.com>
2022-01-17 13:29:48 -0300

    feat: adiciona imagem e titulo da sessao e cria estilos

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Cesta } from './src/telas/cesta';
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import { Cesta } from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Oi eu sou a Biamah! Gatinha</Text>
+    <View>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
